57c3f17cf8811413411ba2be502115e6ad6c11c1
anandjaisy <anandjaisy@gmail.com>
2023-07-02 22:34:33 +1000

    Initial commit

--- infrastructure/src/main/java/fete/bird/OrderRepository.java
+++ infrastructure/src/main/java/fete/bird/OrderRepository.java
@@ -1,13 +1,21 @@
 package fete.bird;
 
 import fete.bird.entity.Order;
+import fete.bird.entity.RootContainer;
 import fete.bird.interfaces.IOrderRepository;
+import io.micronaut.microstream.RootProvider;
 import jakarta.inject.Singleton;
 
 import java.util.UUID;
 
 @Singleton
 public class OrderRepository implements IOrderRepository {
+    private final RootProvider<RootContainer> rootProvider;
+
+    public OrderRepository(RootProvider<RootContainer> rootProvider) {
+        this.rootProvider = rootProvider;
+    }
+
     @Override
     public Order create(Order order) {
         var test =new Order(UUID.randomUUID(), "Hello", "Test", "asdasd");
